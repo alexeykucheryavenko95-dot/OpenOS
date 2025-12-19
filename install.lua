c022d9b4abae8f488d690fc12528857d3b0788f0
kenshi <alexeykucheryavenko95@gmail.com>
2025-12-19 12:49:02 +0200

    Add tps_hud.lua to the files list

--- install.lua
+++ install.lua
@@ -2,6 +2,7 @@ local files = {
   "auto_daemon.lua",
   "hud_daemon.lua",
   "energy_daemon.lua"
+  "tps_hud.lua"
 }
 
 local base = "https://raw.githubusercontent.com/alexeykucheryavenko95-dot/Open-OS/main/"
